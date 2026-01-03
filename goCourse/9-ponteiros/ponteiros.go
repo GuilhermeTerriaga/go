c6a0feb0e45287d725a942b6090eb05be0acdeac
Guilherme Terriaga <guilherme.terriaga@gmail.com>
2026-01-03 16:59:05 -0300

    introducao a ponteiro go

---
+++ goCourse/9-ponteiros/ponteiros.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var1 := 10
+	var2 := var1
+
+	fmt.Println(var1, var2)
+
+	var1++
+
+	fmt.Println(var1, var2)
+	var pont *int
+	pont = &var1
+
+	fmt.Printf("pont: %v\n", pont) // aqui mostra apenas o endereco de memória do ponteiro
+	var1++
+	fmt.Printf("pont: %v\n", *pont) // aqui usando o * a gente manda o ponteiro buscar o valor do endereço
+}
