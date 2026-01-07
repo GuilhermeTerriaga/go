932aa467c6fd9aa50d3c0d804d3358b14d9696c8
Guilherme Terriaga <guilherme.terriaga@gmail.com>
2026-01-07 12:44:05 -0300

    loops completos

--- goCourse/13-loops/loops.go
+++ goCourse/13-loops/loops.go
@@ -7,21 +7,52 @@ import (
 
 func main() {
 	fmt.Println("Loops")
-	i:=0
+	i := 0
 	for i < 10 {
 		i++
 		fmt.Println("Incrementando i ", i)
-		time.Sleep(time.Second)
+		// time.Sleep(time.Second)
 	}
-	for j := 0; j < 10; j++{
+	for j := 0; j < 10; j++ {
 		fmt.Println(j)
-		time.Sleep(time.Second)
+		// time.Sleep(time.Second)
 	}
-	for k := range 10{
+	for k := range 10 {
 		fmt.Println("valor de k ", k)
 	}
+	fmt.Println("array")
 	array := [3]string{"Nome1", "Nome2", "Nome3"}
-	for _, nome := range array{
+	for _, nome := range array {
 		fmt.Println(nome)
+	}
+	fmt.Println("slice")
+	slice := []string{"Nome1", "Nome2", "Nome3"}
+	for _, nome := range slice {
+		fmt.Println(nome)
+	}
+	fmt.Println("por sobre um string")
+	for _, letra := range "PALAVRA" {
+		fmt.Println(letra) // ->> aqui vai trazer a rune da letra
+		fmt.Println(string(letra))
+	}
+	users := map[string]string{
+		"nome":      "zé",
+		"sobrenome": "sobrenome lol",
+	}
+	for chave, valor := range users {
+		println(chave, valor)
+	}
+	type userStruct struct {
+		nome      string
+		sobrenome string
+	}
+	// não é possível iterar sobre um struct
+	// usuario := userStruct{"zé", "sobrenome do zé"}
+	// for chave, valor := range usuario{
+	// 	fmt.Println(chave, valor)
+	// }
+	for {
+		fmt.Println("Infinito")
+		time.Sleep(time.Second)
 	}
 }
